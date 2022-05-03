cb1420e2837c7bfd83ce7c38d2fe8b56886a2aa8
valaqua2007 <quanduongforworks2007@gmail.com>
2022-05-03 15:56:20 +0700

    update code index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import {createConnection} from "./config/connect";
-import { Users } from "./models/users";
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
@@ -14,19 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 createConnection();
-app.post("/", async (req, _res) => {
-  const {username, password} = req.body;
-  try {
-    console.log({username, password});
-    const jane = await Users.create({ username, password});
-    console.log(jane)
-  }catch(e){
-    console.log(e)
-  }
- 
-  // console.log("Jane's auto-generated ID:", jane.id);
-
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
